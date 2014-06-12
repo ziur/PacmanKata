fb8c61a60a24730379fa6534b8135c0c67d6fe38
Alejandro Ruiz <aruiz@pros.com>
2014-06-12 12:58:32 -0400

    Initiail commit

---
+++ src/test/java/PacmanTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by alejandro^ruiz on 5/22/14.
+ */
+public class PacmanTest {
+
+    @Test
+    public void testPacman(){
+        assertEquals(true, true);
+    }
+}
